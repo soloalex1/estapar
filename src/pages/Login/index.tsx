b75fac49197db5cd925c58a6eee0f289f1d80691
Alexandre Machado <amch.smd@gmail.com>
2026-05-27 20:59:55 -0300

    feat(login): login screen layout

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,6 +3,10 @@ import { useNavigate } from 'react-router-dom';
 
 import { AuthContext } from '../../contexts/AuthContext';
 
+import Input from '../../components/Input';
+
+import EstaparLogo from '../../assets/logo.svg?react';
+
 export function LoginPage() {
   const { login } = use(AuthContext)!;
   const navigate = useNavigate();
@@ -24,13 +28,46 @@ export function LoginPage() {
   );
 
   return (
-    <form action={submitAction}>
-      <input type="email" name="email" placeholder="E-mail" required />
-      <input type="password" name="password" placeholder="Senha" required />
-      {error && <p>{error}</p>}
-      <button type="submit" disabled={isPending}>
-        {isPending ? 'Entrando...' : 'Entrar'}
-      </button>
-    </form>
+    <main
+      aria-label="Página de login"
+      className="w-dvw h-dvh p-4 md:p-0 bg-surface flex items-center justify-center"
+    >
+      <div className="w-full md:w-1/2 border border-border rounded-lg p-4 md:p-8">
+        <EstaparLogo className="w-32 md:w-60 h-auto mb-6 mx-auto" />
+
+        <h1 className="text-black font-semibold text-md mb-4">
+          Entre com suas credenciais para acessar o sistema
+        </h1>
+        <form action={submitAction} className="w-full mx-auto">
+          <Input
+            type="email"
+            name="email"
+            id="email"
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            required
+          />
+
+          <Input
+            type="password"
+            name="password"
+            id="password"
+            label="Senha"
+            placeholder="Digite sua senha"
+            required
+          />
+
+          {error && <p>{error}</p>}
+
+          <button
+            type="submit"
+            disabled={isPending}
+            className="bg-brand text-white py-2 px-4 rounded w-full"
+          >
+            {isPending ? 'Entrando...' : 'Entrar'}
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
