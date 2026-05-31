3871f08948a22e980c915a8d0c0c25a94d5484da
Alexandre Machado <amch.smd@gmail.com>
2026-05-31 12:47:52 -0300

    feat(garages): go back button

--- src/pages/Garages/index.tsx
+++ src/pages/Garages/index.tsx
@@ -1,5 +1,9 @@
 import { use } from 'react';
-import { BuildingOffice2Icon } from '@heroicons/react/24/outline';
+import { useNavigate } from 'react-router-dom';
+import {
+  BuildingOffice2Icon,
+  ArrowLeftIcon,
+} from '@heroicons/react/24/outline';
 
 import Table from '../../components/Table';
 import Pagination from '../../components/Pagination';
@@ -8,13 +12,25 @@ import useGarages from '../../hooks/useGarages';
 import SidebarContext from '../../contexts/SidebarContext';
 
 const GaragesPage = () => {
+  const navigate = useNavigate();
   const { isCollapsed } = use(SidebarContext)!;
+
+  const goBack = () => {
+    navigate(-1);
+  };
 
   return (
     <section
       aria-labelledby="garages-title"
       className={`w-full h-full mt-16 lg:mt-20 flex flex-col items-start justify-start gap-2 pl-22 pr-4 ${isCollapsed ? 'md:px-24' : 'md:pl-72 md:pr-16'} transition-all duration-300`}
     >
+      <button
+        onClick={goBack}
+        className="hidden md:inline-flex gap-2 items-center text-gray-500 mb-2 py-2 px-4 text-sm border-2 border-transparent hover:border-brand rounded-sm transition-colors duration-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand "
+      >
+        <ArrowLeftIcon className="w-4 h-4" />
+        Voltar
+      </button>
       <div className="flex gap-2 justify-start items-center">
         <BuildingOffice2Icon className="w-9 h-9 text-brand" />
         <h1 id="garages-title" className="text-3xl text-black font-bold">
